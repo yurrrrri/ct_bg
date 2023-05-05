fb05a1e9db93639e79bc33f7e342803f49f4fb81
yurrrrri <yu_ri_son@naver.com>
2023-05-05 16:02:11 +0900

    p10430

---
+++ src/main/java/com/ll/bronze/bronze5/p10430/Main.java
@@ -0,0 +1,20 @@
+package com.ll.bronze.bronze5.p10430;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C) % C));
+
+        sc.close();
+    }
+}
---
+++ src/main/java/com/ll/bronze/bronze5/p18108/Main.java
@@ -0,0 +1,12 @@
+package com.ll.bronze.bronze5.p18108;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year - 543);
+        sc.close();
+    }
+}
