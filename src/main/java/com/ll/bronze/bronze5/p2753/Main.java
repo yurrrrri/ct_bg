101b8cb68232f7ba150c1fc1eee5988ea79329d1
yurrrrri <yu_ri_son@naver.com>
2023-05-06 01:23:02 +0900

    p2753

---
+++ src/main/java/com/ll/bronze/bronze5/p2753/Main.java
@@ -0,0 +1,16 @@
+package com.ll.bronze.bronze5.p2753;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) System.out.println("1");
+        else System.out.println("0");
+
+        sc.close();
+    }
+}
