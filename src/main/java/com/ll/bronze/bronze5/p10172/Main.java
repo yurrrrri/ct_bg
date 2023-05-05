ebc3f8422f09d9b5dd68f946d128ec97c8126cba
yurrrrri <yu_ri_son@naver.com>
2023-05-06 01:11:22 +0900

    p10172

---
+++ src/main/java/com/ll/bronze/bronze5/p10172/Main.java
@@ -0,0 +1,13 @@
+package com.ll.bronze.bronze5.p10172;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                |\\_/|
+                |q p|   /}
+                ( 0 )""\"\\
+                |"^"`    |
+                ||_/=\\\\__|
+                """);
+    }
+}
