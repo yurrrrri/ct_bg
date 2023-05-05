4de5d2841ba760d0642b4975de83c3eee38c4f6f
yurrrrri <yu_ri_son@naver.com>
2023-05-05 15:55:03 +0900

    p11660, p10926

---
+++ src/main/java/com/ll/bronze/bronze5/p10926/Main.java
@@ -0,0 +1,12 @@
+package com.ll.bronze.bronze5.p10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+        sc.close();
+    }
+}
