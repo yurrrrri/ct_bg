1963b1bedbcb210accfe665d875bb084583e74bf
yurrrrri <yu_ri_son@naver.com>
2023-05-06 01:10:30 +0900

    p10171

---
+++ src/main/java/com/ll/bronze/bronze5/p10171/Main.java
@@ -0,0 +1,12 @@
+package com.ll.bronze.bronze5.p10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                \\    /\\
+                 )  ( ')
+                (  /  )
+                 \\(__)|
+                """);
+    }
+}
