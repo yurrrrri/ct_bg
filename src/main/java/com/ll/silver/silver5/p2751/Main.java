a7c1b3c8e20f93b845b66daff0be2bee48964409
yurison <yu_ri_son@naver.com>
2023-08-03 15:34:31 +0900

    p2751 - 병합 정렬을 이용한 풀이

--- src/main/java/com/ll/silver/silver5/p2751/Main.java
+++ src/main/java/com/ll/silver/silver5/p2751/Main.java
@@ -1,32 +1,71 @@
 package com.ll.silver.silver5.p2751;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
+    // p2750과 같음
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        List<Integer> list = new ArrayList<>();
+//        for(int i=0; i<n; i++){
+//            list.add(Integer.parseInt(br.readLine()));
+//        }
+//        Collections.sort(list);
+//
+//        for(int i=0; i<list.size(); i++){
+//            bw.write(list.get(i) + "\n");
+//        }
+//
+//        bw.flush();
+//
+//        br.close();
+//        bw.close();
+//    }
+
+    public static int[] arr, temp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        temp = new int[N + 1];
 
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            list.add(Integer.parseInt(br.readLine()));
-        }
-        Collections.sort(list);
+        for (int i = 1; i <= N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<list.size(); i++){
-            bw.write(list.get(i) + "\n");
-        }
+        mergeSort(1, N);
+
+        for (int k = 1; k <= N; k++)
+            bw.write(arr[k] + "\n");
 
         bw.flush();
-
-        br.close();
         bw.close();
     }
+
+    private static void mergeSort(int start, int end) {
+        if (end - start < 1) return;
+
+        int middle = start + (end - start) / 2;
+        mergeSort(start, middle);
+        mergeSort(middle + 1, end);
+
+        for (int i = start; i <= end; i++) temp[i] = arr[i];
+
+        int t = start;
+        int i = start;
+        int j = middle + 1;
+
+        while (i <= middle && j <= end) {
+            if (temp[i] > temp[j]) arr[t++] = temp[j++];
+            else arr[t++] = temp[i++];
+        }
+
+        while (i <= middle) arr[t++] = temp[i++];
+        while (j <= end) arr[t++] = temp[j++];
+    }
+
 }
- /*
- p2750과 같음
-  */
