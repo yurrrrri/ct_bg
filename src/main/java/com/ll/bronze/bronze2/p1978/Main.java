8fff93317978d016ad7d493b63f9932f578f3bcd
yurison <yu_ri_son@naver.com>
2023-08-04 11:07:43 +0900

    p1978

---
+++ src/main/java/com/ll/bronze/bronze2/p1978/Main.java
@@ -0,0 +1,35 @@
+package com.ll.bronze.bronze2.p1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            boolean isPrime = true;
+
+            if (num == 1) continue;
+
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) result++;
+        }
+
+        System.out.println(result);
+    }
+}
