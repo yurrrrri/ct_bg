2b876c3174c210560425b831f34edf8ca011126a
yurison <yu_ri_son@naver.com>
2023-08-07 03:11:04 +0900

    p1920 - 이진 탐색을 이용한 풀이(시간초과)

--- src/main/java/com/ll/silver/silver4/p1920/Main.java
+++ src/main/java/com/ll/silver/silver4/p1920/Main.java
@@ -3,6 +3,7 @@ package com.ll.silver.silver4.p1920;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,7 +15,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String[] arr1 = br.readLine().split(" ");
 
-        for(String s : arr1) set.add(s);
+        Collections.addAll(set, arr1);
 
         int m = Integer.parseInt(br.readLine());
         String[] arr2 = br.readLine().split(" ");
@@ -26,4 +27,46 @@ public class Main {
 
         br.close();
     }
+
+    // 이진 탐색을 이용한 풀이
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//
+//        int[] arr = new int[N];
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        Arrays.sort(arr);
+//
+//        int M = Integer.parseInt(br.readLine());
+//        st = new StringTokenizer(br.readLine());
+//
+//        for (int i = 0; i < M; i++) {
+//            boolean found = false;
+//            int target = Integer.parseInt(st.nextToken());
+//
+//            int start = 0;
+//            int end = arr.length - 1;
+//
+//            while (start <= end) {
+//                int midIndex = (start + end) / 2;
+//                int midValue = arr[midIndex];
+//
+//                if (midValue > target)
+//                    end = midIndex;
+//                else if (midValue < target)
+//                    start = midIndex + 1;
+//                else {
+//                    found = true;
+//                    break;
+//                }
+//            }
+//
+//            if (found) System.out.println(1);
+//            else System.out.println(0);
+//        }
+//    }
 }
